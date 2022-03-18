01e55f0292128a7897c56c2275e423142a2322ad
Shezad <shezad.tufail@hotmail.com>
2022-03-18 21:54:25 +0800

    update shell

---
+++ shell/linux_setup.sh
@@ -0,0 +1,11 @@
+# Download Chrome, VScode, Anaconda, RStudio
+
+# R
+
+    # Install dependencies required to install R packages such as tidyverse
+sudo apt-get install libssl-dev
+sudo apt-get install libcurl4-openssl-dev
+sudo apt-get install libxml2-dev 
+
+# GIT
+sudo apt install git
