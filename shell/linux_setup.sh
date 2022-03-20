4ce8b7eab14706a9abd102f774c630b0d9c8cc4a
Shezad <shezad.tufail@hotmail.com>
2022-03-20 08:12:21 +0800

    update

--- shell/linux_setup.sh
+++ shell/linux_setup.sh
@@ -7,6 +7,11 @@ sudo apt-get install libssl-dev
 sudo apt-get install libcurl4-openssl-dev
 sudo apt-get install libxml2-dev 
 
+# PYTHON
+
+    # Create a virtual environnement called ds_env
+conda create -n ds_env python=3.9 numpy pandas scikit-learn jupyter seaborn
+
 # GIT
 sudo apt install git
 
@@ -15,3 +20,6 @@ sudo apt install git
     # Shared folder access
 sudo apt-get install virtualbox-guest-utils
 sudo adduser $USER vboxsf
+
+# FONTS
+sudo apt install fonts-firacode
