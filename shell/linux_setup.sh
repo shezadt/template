a70dd1b620ccee95c04a3fc2cbeb16087243d54b
shezadt <shezad.tufail@gmail.com>
2022-04-28 12:51:22 +0800

    update

--- shell/linux_setup.sh
+++ shell/linux_setup.sh
@@ -2,6 +2,12 @@
 
 # R
 
+    # Install R on Ubuntu 20.04
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+sudo apt update
+sudo apt install r-base
+
     # Install dependencies required to install R packages such as tidyverse
 sudo apt-get install libssl-dev
 sudo apt-get install libcurl4-openssl-dev
