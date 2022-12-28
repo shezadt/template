9f6eb03a5178702af0e39f915ab796b942e57c6c
shzt <shezad.tufail@hotmail.com>
2022-12-28 13:46:18 +0800

    Update cheat_sheet.py

--- python/cheat_sheet.py
+++ python/cheat_sheet.py
@@ -1,13 +1,23 @@
-# Load the libraries
+# load the libraries
 import pandas as pd
 import numpy as np
+from datetime import datetime, timedelta, date
 
-# Initialize a dataframe
+# initialize a dataframe
 df = pd.DataFrame({'num_legs': [2, 4, 4, 6, np.NaN],
                    'num_wings': [2, 0, 0, 0, 7]})
 
-# Return null data
+# return null data
 null_data = df[df.isnull().any(axis=1)]
 
-# Get unique values of a column
+# get unique values of a column
 df['num_wings'].value_counts()
+
+# get today date
+today_date = date.today()
+
+# minus days from a date, e.g. 3
+start_date = today_date - timedelta(days=3)
+
+# replace the day by another day, e.g. 1
+date_replaced = today_date.replace(day=1)
