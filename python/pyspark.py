499155a372947a373af57683e6d8dc632af993aa
shzt <shezad.tufail@hotmail.com>
2022-12-28 15:38:23 +0700

    Update pyspark.py

--- python/pyspark.py
+++ python/pyspark.py
@@ -1,6 +1,8 @@
 # load the libraries
 import pyspark.sql.functions as f
 
+# CONNECTION / READING
+
 # connect to postgre sql server
 source_data = (
     spark.read.format("jdbc")
@@ -21,8 +23,15 @@ source_data = spark.read.parquet(parquet_path)
 # read csv file
 source_data = spark.read.option("header", "true").csv(csv_path)
 
+# SQL
+
 # transform a sql query into a spark df
 results = sqlContext.sql('''SELECT * FROM xxx''')
+
+# create a view from a spark df
+s_df.createOrReplaceTempView("view_name")
+
+# PROCESSING
 
 # transform a column into a list
 list_ids = s_df.select("id").rdd.flatMap(lambda x: x).collect()
